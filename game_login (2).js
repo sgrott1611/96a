924eef7bc7f33f9aa35b7769099a7f94986742f3
sgrott1611 <139928512+sgrott1611@users.noreply.github.com>
2023-12-21 12:24:03 -0300

    Add files via upload

---
+++ game_login (2).js
@@ -0,0 +1,6 @@
+// Crie a função addUser()
+function addUser() {
+  // Obtenha o valor do usuário através das ids player1_name_input e player2_name_input
+  // Armazene esses valores localmente
+  // Atribua "game_page.html" para window.location
+}
